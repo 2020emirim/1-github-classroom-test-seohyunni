bb436c71f1bc741a5c02387c367f61f06a6f66f4
shyunni <s2018w35@e-mirim.hs.kr>
2020-05-13 09:07:55 +0900

    admin ChoiceInline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
 
-class Questionadmin(admin.ModelAdmin):
-    field = ["pub_date", "question_text"]
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
-admin.site.register[Question, Questionadmin]
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
+admin.site.register(Question, QuestionAdmin)
